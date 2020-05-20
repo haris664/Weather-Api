e5749b3d63a079aa3adefe1aa06fb6e415cfc272
Haris Munir <haris4000@yahoo.com>
2020-05-20 01:09:37 -0500

    set all the files in the src directory inorder to apply the gulp functionality in the dest folder

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const cleanCss = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const babel1 = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+
+function html() {
+  return gulp.src()
+}
